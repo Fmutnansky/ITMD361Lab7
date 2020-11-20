c28c53eff5f91c326028d2fb2375ca2962e696fa
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2020-11-19 23:12:57 -0600

    Update script.js
    Trying to fix button not working

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function init(){
   var entry = document.getElementById('entryinput');
   var button = document.getElementById('entrybutton');
   var txoutput = document.getElementById('textoutput');
-  button.addEventListener('click', function()){
+  button.addEventListener('click', function(){
     alert('Fredrick Mutnansky: ' + entry.value);
     txoutput.innerHTML = entry.value;
   });
