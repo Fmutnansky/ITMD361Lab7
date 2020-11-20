3ac30ee15c0ba3f754276c99f9fb6cc0020f76e0
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2020-11-19 23:19:17 -0600

    Update script.js
    Trying to fix button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
-  var entry = document.getElementById('entryinput');
-  var button = document.getElementById('entrybutton');
-  var txoutput = document.getElementById('textoutput');
-  button.addEventListener('click', function(){
-    alert('Fredrick Mutnansky: ' + entry.value);
-    txoutput.innerHTML = entry.value;
-  });
+var entry = document.getElementById('entryinput');
+var button = document.getElementById('entrybutton');
+var txoutput = document.getElementById('textoutput');
+button.addEventListener('click', function(){
+alert('Fredrick Mutnansky: ' + entry.value);
+txoutput.innerHTML = entry.value;
+});
 }
 window.addEventListener('load', init);
